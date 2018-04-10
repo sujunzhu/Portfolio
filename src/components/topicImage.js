368e104aa85d31861016043e78f22b1d8d06da2f
Sujun Zhu <sujunzhu@usc.edu>
2018-04-09 23:10:27 -0700

    add_content

--- src/components/topicImage.js
+++ src/components/topicImage.js
@@ -2,15 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const TopicImage = ({text,img,url,type}) => {
-  const TopicImageCss = {
-    background: `url(${img}) no-repeat center center`,
-    backgroundSize: `cover`
-  }
   return (
-    <div className={type} style={TopicImageCss}>
+    <div className={type}>
       <Link to={url} target="_blank">
-        <div>
-        </div>
+        <img src={img} className="specifixImage"/>
       </Link>
     </div>
   )
